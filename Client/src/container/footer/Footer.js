be5d3dbd03892116dbb48fa8987b1e45335b0b1c
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-12-06 16:18:15 +0300

    Update Footer.js

--- Client/src/container/footer/Footer.js
+++ Client/src/container/footer/Footer.js
@@ -3,7 +3,7 @@ import "./footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-            <p>&copy;2021, All rights reserved!</p>
+            <p>&copy;2022, All rights reserved!</p>
         </div>
     )
 }
